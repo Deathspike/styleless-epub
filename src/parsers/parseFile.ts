1b8c4efa2df6cf64f56f10a4aff9025c94c78cef
Deathspike <meister.deathspike@outlook.com>
2020-10-25 09:06:15 +0100

    Add css backup to epub files

--- src/parsers/parseFile.ts
+++ src/parsers/parseFile.ts
@@ -6,10 +6,11 @@ export async function parseFileAsync(filePath: string) {
   const zip = new AdmZip(filePath);
   const zipEntries = zip.getEntries();
   for (const zipEntry of zipEntries) {
-    if (!zipEntry.name.endsWith('.css')) continue;
+    if (!zipEntry.entryName.endsWith('.css')) continue;
     const originalCss = zipEntry.getData().toString('utf8');
     const modifiedCss = app.parseStyle(originalCss);
     zip.updateFile(zipEntry, Buffer.from(modifiedCss, 'utf8'));
+    zip.addFile(`${zipEntry.entryName}.old`, Buffer.from(originalCss, 'utf8'))
   }
   await util.promisify(zip.writeZip)(undefined);
 }
