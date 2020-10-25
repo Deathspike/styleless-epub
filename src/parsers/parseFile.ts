39b5f2ddf83a389806e6fc03745a4e18050c88ac
Deathspike <meister.deathspike@outlook.com>
2020-10-25 09:50:46 +0100

    Fix parser emitting backup file more than once

--- src/parsers/parseFile.ts
+++ src/parsers/parseFile.ts
@@ -5,14 +5,22 @@ import util from 'util';
 export async function parseFileAsync(filePath: string) {
   console.log(`Fetching ${filePath}`);
   const zip = new AdmZip(filePath);
-  const zipEntries = zip.getEntries();
-  for (const zipEntry of zipEntries) {
+  const zipEntries = recordSet(zip.getEntries());
+  for (const zipEntry of Object.values(zipEntries)) {
     if (!zipEntry.entryName.endsWith('.css')) continue;
+    const backupName = `${zipEntry.entryName}.old`;
     const originalCss = zipEntry.getData().toString('utf8');
     const modifiedCss = app.parseStyle(originalCss);
     zip.updateFile(zipEntry, Buffer.from(modifiedCss, 'utf8'));
-    zip.addFile(`${zipEntry.entryName}.old`, Buffer.from(originalCss, 'utf8'))
+    if (zipEntries.hasOwnProperty(backupName)) continue;
+    zip.addFile(backupName, Buffer.from(originalCss, 'utf8'))
   }
   await util.promisify(zip.writeZip)(undefined);
   console.log(`Finished ${filePath}`);
+}
+
+function recordSet(entries: AdmZip.IZipEntry[]) {
+  const result: Record<string, AdmZip.IZipEntry> = {};
+  entries.forEach(x => result[x.entryName] = x);
+  return result;
 }
